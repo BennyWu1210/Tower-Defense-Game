3ea2092de0a6727ae2c0a2d96d1dfb62253922b6
bennywu2019 <benny.wu.new@gmail.com>
2020-12-11 13:53:46 -0500

    testing
    testing

--- ICS3U_Final_Project-copy/MyWorld.java
+++ ICS3U_Final_Project-copy/MyWorld.java
@@ -17,6 +17,6 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        //HELOSDFJSDKLDSF
+        //HELO
     }
 }
