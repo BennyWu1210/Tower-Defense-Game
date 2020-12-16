5b2416fc06b781a97cfd88ff4bba99d6c8cefd72
bennywu2019 <benny.wu.new@gmail.com>
2020-12-16 16:24:21 -0500

    Background designs
    This LITERALLY took me a whole day to draw. Oh god I shouldn't have decided to design all of these myself.

--- ICS3U_Final_Project/MyWorld.java
+++ ICS3U_Final_Project/MyWorld.java
@@ -8,29 +8,35 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    GreenfootImage background = new GreenfootImage("images/game_map06.tif");
+    GreenfootImage test = new GreenfootImage("images/testingab.png");
+    GreenfootImage a = new GreenfootImage("images/inferno_tower1.png");
+    GreenfootImage b = new GreenfootImage("images/lightning_tower01.tif");
     /**
-     * Constructor for objects of class MyWorld.
+     * Constructor for objects of class MyWorld
      * 
      */
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
-        //HELOSDFJSDKLDSF
-        //WHASSSSSSSUP
-        //LEMME TEST
-        //YEEEEEEEE
-        //FINAAAALLY
-        //yeeee
-        ///yapsss
-        //sdsd
-        //sudhusd
-        //sdsdsds
-        //lemme actually write some code
+        super(1050, 700, 1); 
+        setBackground(background);
+        background.scale(1050, 700);
+        displayTest(350, 100);
+        a.scale(80,120);
+        background.drawImage(a, 300, 250);
+        b.scale(80,120);
+        background.drawImage(b, 470, 250);
+    }
+    
+    public void displayTest(int x, int y)
+    {
+        test.scale(100,100);
+        background.drawImage(test, x, y);
     }
     public void add()
     {
         ;
     }
+    
 }
