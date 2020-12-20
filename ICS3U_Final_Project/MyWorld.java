bc9c9f714a9bb8c2eb8fea3b8f241a2fbb19a96a
bennywu2019 <benny.wu.new@gmail.com>
2020-12-19 20:33:54 -0500

    Path finding
    Lol spent so long fixing this: Using the mouse-clicking method to locate the path(requires optimization) and allows the enemy to move along the path

--- ICS3U_Final_Project/MyWorld.java
+++ ICS3U_Final_Project/MyWorld.java
@@ -1,4 +1,7 @@
+
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
+import java.io.*;
 
 /**
  * Write a description of class MyWorld here.
@@ -8,10 +11,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-    GreenfootImage background = new GreenfootImage("images/game_map06.tif");
-    GreenfootImage test = new GreenfootImage("images/testingab.png");
-    GreenfootImage a = new GreenfootImage("images/inferno_tower1.png");
-    GreenfootImage b = new GreenfootImage("images/lightning_tower01.tif");
+    GreenfootImage background = new GreenfootImage("images/game_map08.png");
+    
+    
+    public static ArrayList<int[]> pathOne = new ArrayList<int[]>();
+    static ArrayList<int[]> pathTwo = new ArrayList<int[]>();
+    
     /**
      * Constructor for objects of class MyWorld
      * 
@@ -22,21 +27,89 @@ public class MyWorld extends World
         super(1050, 700, 1); 
         setBackground(background);
         background.scale(1050, 700);
-        displayTest(350, 100);
-        a.scale(80,120);
-        background.drawImage(a, 300, 250);
-        b.scale(80,120);
-        background.drawImage(b, 470, 250);
+        System.out.println("--------");
+        System.out.println(pathOne.size());
+        
+        readMouseInfo("Tower Defense MousePos1.txt", pathOne);
+        readMouseInfo("Tower Defense MousePos2.txt", pathTwo);
+        
+        DudeEnemy aa = new DudeEnemy(16, 10, 10);
+        addObject(aa, pathOne.get(0)[0], pathOne.get(0)[1]);
+        aa.relocate(pathOne.get(0)[1],pathOne.get(0)[1]);
+        
+        
+        
+        
+
+        /*
+        for(int[] arr: pathOne)
+        {
+            System.out.println(arr[0] + " " + arr[1]);
+        }
+        */
     }
     
-    public void displayTest(int x, int y)
+    public void act()
     {
-        test.scale(100,100);
-        background.drawImage(test, x, y);
-    }
-    public void add()
-    {
+
         ;
     }
+
+    public void mouseCoords(String f)
+    {
+        if(Greenfoot.mouseClicked(null))
+        {
+            int x = Greenfoot.getMouseInfo().getX();
+            int y = Greenfoot.getMouseInfo().getY();
+            int[] arr = {x,y};
+            System.out.println(x + " " + y);
+            writeMouseInfo(x,y,f);
+        }
+    }
     
+    public void writeMouseInfo(int x, int y, String f)
+    {
+        try
+        {
+            String file = f;
+            FileWriter writer = new FileWriter(file, true);
+            BufferedWriter myWriter = new BufferedWriter(writer);
+            myWriter.write("(" + x + ", " + y + ")");
+            myWriter.newLine();
+            myWriter.close();
+            System.out.println("written!");
+        }
+        
+        catch(IOException e)
+        {
+            System.out.println("An error occured.");
+            e.printStackTrace();
+        }
+        
+    }
+    
+    public void readMouseInfo(String f, ArrayList<int[]> path)
+    {
+        try
+        {
+            
+            File file = new File(f);
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine())
+            {
+                String data = sc.nextLine();
+                int x = Integer.parseInt(data.substring(1, 4));
+                int y = Integer.parseInt(data.substring(6, 9));
+                int[] arr = {x, y};
+                path.add(arr);
+                // TODO: There may be two-digit integers, not the best way!
+            }
+            
+        }
+        
+        catch(IOException e)
+        {
+            System.out.print("Cannot read file");
+        }
+    }
 }
